d7cab85e08774e60c1d34eabf97092b25f19f64e
Chidiebere Okpala <okpalacollins4@gmail.com>
2021-01-19 17:51:39 -0600

    Slider input in React Native
    Note: the Slider we used here is depreciated.
    But you can get it from @react-native-community/slider
    Check theor github for more info!

--- App.js
+++ App.js
@@ -1,12 +1,35 @@
 import React from 'react'
-import { View } from 'react-native'
+import { View, StyleSheet, Text, Slider } from 'react-native'
 import AddEntry from './components/AddEntry'
 
-export default function App() {
+export default class App extends React.Component{
 
-  return (
-    <View >
-      <AddEntry />
-    </View>
-  )
+  state = {
+    value: 0
+  }
+
+  render(){
+    return (
+      <View style={styles.container}>
+        <Slider
+        minimumValue={-10}
+        maximumValue={10}
+        step={//to remove the decimals
+          1}
+        value={this.state.value}
+        onValueChange={(value) => this.setState({value})}
+        />
+        <Text>Value: {this.state.value}</Text>
+      </View>
+    )
+  }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'stretch', //to make the slider have full width of screen
+    justifyContent: 'center',
+  },
+});
