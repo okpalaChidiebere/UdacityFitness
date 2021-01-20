20eb3cf38e1ee719690d46addcbe17dcb21a2b1d
Chidiebere Okpala <okpalacollins4@gmail.com>
2021-01-20 14:47:32 -0600

    Styling AddEntry Component - 1
    We want our buttons(eg increasment and decreament button) and icons to look different depending on the platform(OS)

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react"
-import { View, TouchableOpacity, Text } from "react-native"
+import { View, TouchableOpacity, Text, StyleSheet ,Platform } from "react-native"
 import { getMetricMetaInfo, timeToString, getDailyReminderValue } from "../utils/helpers"
 import UdaciSlider from './UdaciSlider'
 import UdaciSteppers from './UdaciSteppers'
@@ -9,6 +9,7 @@ import TextButton from './TextButton'
 import { submitEntry, removeEntry } from '../utils/api'
 import { connect } from 'react-redux'
 import { addEntry } from '../actions'
+import { purple, white } from "../utils/colors";
 
 
 
@@ -16,9 +17,12 @@ import { addEntry } from '../actions'
 function SubmitBtn ({ onPress }) {
   return (
     <TouchableOpacity
+    style={
+      Platform.OS === "ios" ? styles.iosSubmitBtn : styles.AndroidSubmitBtn
+    }
       onPress={//note: 'onPress' in React native is equivalent to onClick in normal React
         onPress}>
-        <Text>SUBMIT</Text>
+        <Text style={styles.submitBtnText}>SUBMIT</Text>
     </TouchableOpacity>
   )
 }
@@ -111,17 +115,17 @@ class AddEntry extends Component {
 
     if (this.props.alreadyLogged) { //eventually we will pass this.props.alreadyLogged
       return (
-        <View>
-          <Ionicons name={"md-happy-outline"} size={100} />
+        <View style={styles.center}>
+          <Ionicons name={Platform.OS === "ios" ? "ios-happy-outline" : "md-happy-outline"} size={100} />
           <Text>You already logged your information for today.</Text>
-          <TextButton onPress={this.reset}>Reset</TextButton>
+          <TextButton style={{ padding: 10 }} onPress={this.reset}>Reset</TextButton>
         </View>
       );
     }
 
 
     return (
-      <View>
+      <View style={styles.container}>
         <DateHeader date={(new Date()).toLocaleDateString()}/>
         <Text>{ //for debugging
         /*JSON.stringify(this.state)*/}</Text>
@@ -130,7 +134,7 @@ class AddEntry extends Component {
           const value = this.state[key]
 
           return (
-            <View key={key}>
+            <View key={key} style={styles.row}>
               {getIcon()}
               {type === 'slider'
                 ? <UdaciSlider
@@ -152,6 +156,51 @@ class AddEntry extends Component {
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: white
+  },
+  row: { //styling each row for a meta data entry information for this form
+    flexDirection: "row",
+    flex: 1,
+    alignItems: "center"
+  },
+  iosSubmitBtn: {
+    backgroundColor: purple,
+    padding: 10,
+    borderRadius: 7,
+    height: 45,
+    marginLeft: 40,
+    marginRight: 40
+  },
+  AndroidSubmitBtn: {
+    backgroundColor: purple,
+    padding: 10,
+    paddingLeft: 30,
+    paddingRight: 30,
+    height: 45,
+    borderRadius: 2,
+    alignSelf: "flex-end",
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  submitBtnText: {
+    color: white,
+    fontSize: 22,
+    textAlign: "center"
+  },
+  center: { //used for styling the view that will show when the user has already logged theor info
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginLeft: 30,
+    marginRight: 30
+  }
+});
+
 
 function mapStateToProps (state) {
   const key = timeToString()
