2c6eaed0705b9405e6a85412e848586d9c64b227
Chidiebere Okpala <okpalacollins4@gmail.com>
2021-01-19 18:09:41 -0600

    AddEntry-UdaciSlider

--- App.js
+++ App.js
@@ -1,25 +1,13 @@
 import React from 'react'
-import { View, StyleSheet, Text, Slider } from 'react-native'
+import { View, StyleSheet } from 'react-native'
 import AddEntry from './components/AddEntry'
 
 export default class App extends React.Component{
 
-  state = {
-    value: 0
-  }
-
   render(){
     return (
       <View style={styles.container}>
-        <Slider
-        minimumValue={-10}
-        maximumValue={10}
-        step={//to remove the decimals
-          1}
-        value={this.state.value}
-        onValueChange={(value) => this.setState({value})}
-        />
-        <Text>Value: {this.state.value}</Text>
+        <AddEntry/>
       </View>
     )
   }
--- components/UdaciSlider.js
+++ components/UdaciSlider.js
@@ -1,10 +1,18 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, Slider } from 'react-native'
 
-export default function UdaciSlider () {
+export default function UdaciSlider ({ max, unit, step, value, onChange }) {
   return (
     <View>
-      <Text>UdaciSlider</Text>
+        <Slider
+        maximumValue={max}
+        minimumValue={0}
+        step={step}
+        value={value}
+        onValueChange={onChange}
+        />
+      <Text>{value}</Text>
+      <Text>{unit}</Text>
     </View>
   )
 }
