a0a4e6eb7e8d056f5da576b8ef3aa68afb32da93
Chidiebere Okpala <okpalacollins4@gmail.com>
2021-01-21 16:02:14 -0600

    navigationOptions (set title) in React Native
    Add title in the as Header for Entry Details Page
    
    NOTE: Stack Navigator animations render differently in Android and iOS

--- components/EntryDetail.js
+++ components/EntryDetail.js
@@ -15,7 +15,10 @@ class EntryDetail extends Component {
 export default EntryDetail
 
 export function EntryDetailNavigationOptions({ route }) { //we haveaccess to the route props. we can get any parameter we want like ids from this props
-    
+    const { entryId } = route.params
+    const year = entryId.slice(0, 4)
+    const month = entryId.slice(5, 7)
+    const day = entryId.slice(8)
     return{
         //Styling the header for this Component
         headerTintColor: white,
@@ -23,5 +26,6 @@ export function EntryDetailNavigationOptions({ route }) { //we haveaccess to the
             backgroundColor: purple,
         },
         //End Etyling header for this Component
+        title: `${month}/${day}/${year}`, //this ttitle appear at the center of the header with backbutton to the left end of the header
     }
 }
