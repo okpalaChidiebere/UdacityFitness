f25e6e7e79f38caa28c480ff31fc677d86fd4e10
Chidiebere Okpala <okpalacollins4@gmail.com>
2021-01-20 21:00:32 -0600

    Styling Calendar ListItem in History Component
    - We implemented styling for the two methods the react-native-calendar uses to render items from the store
    - We attached an onClick listener for when an item in the calendar is clicked

--- components/History.js
+++ components/History.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet, Platform, TouchableOpacity } from 'react-native'
 import { connect } from 'react-redux'
 import { receiveEntries, addEntry } from '../actions'
 import { timeToString, getDailyReminderValue } from '../utils/helpers'
 import { fetchCalendarResults } from '../utils/api'
 import { Agenda as UdacityFitnessCalendar } from 'react-native-calendars'
+import { white } from '../utils/colors'
+import DateHeader from './DateHeader'
 
 class History extends Component {
 
@@ -28,19 +30,33 @@ class History extends Component {
   }
 
   renderItem = ({ today, ...metrics }, formattedDate, key) => {
-      console.log(today)
+      //console.log(today)
     return(
-    <View>
+    <View style={styles.item}>
       {today
-        ? <Text>{JSON.stringify(today)}</Text>
-        : <Text>{JSON.stringify(metrics)}</Text>}
+        ? <View>
+            <DateHeader date={formattedDate}/>
+            <Text style={styles.noDataText}>
+              {today}
+            </Text>
+          </View>
+        : <TouchableOpacity
+            onPress={() => console.log('Pressed!')}
+          >
+              <Text>{JSON.stringify(metrics)}</Text>
+          </TouchableOpacity>}
     </View>
-  )}
-  renderEmptyDate(formattedDate) { //we use the arrow function, because we are not using the 'this' keyword inside of it
+    )}
+
+  renderEmptyDate(date) { //we use the arrow function, because we are not using the 'this' keyword inside of it
+    const formattedDate = date.toString("MMMM d, yyyy") //convert "2021-01-20T00:00:00.000Z" to Janyary 20, 2021
     return (
-      <View>
-        <Text>No Data for this day</Text>
-      </View>
+        <View style={styles.item}>
+            <DateHeader date={formattedDate}/>
+            <Text style={styles.noDataText}>
+                You didn't log any data on this day.
+            </Text>
+        </View>
     )
   }
 
@@ -57,6 +73,31 @@ class History extends Component {
     )
   }
 }
+
+const styles = StyleSheet.create({
+    item: {
+      backgroundColor: white,
+      borderRadius: Platform.OS === 'ios' ? 16 : 2,
+      padding: 20,
+      marginLeft: 10,
+      marginRight: 10,
+      marginTop: 17,
+      justifyContent: 'center',
+      shadowRadius: 3,
+      shadowOpacity: 0.8,
+      shadowColor: 'rgba(0, 0, 0, 0.24)',
+      shadowOffset: {
+        width: 0,
+        height: 3
+      },
+    },
+    noDataText: {
+      fontSize: 20,
+      paddingTop: 20,
+      paddingBottom: 20
+    }
+})  
+
 
 function mapStateToProps (entries) {
   return {
