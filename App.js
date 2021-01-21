e535fd8cd57724a7c6eef2197da685379382ee26
Chidiebere Okpala <okpalacollins4@gmail.com>
2021-01-21 10:30:21 -0600

    Tab Navigator inreact Native
    We end up installing @react-navigation/native @react-navigation/material-top-tabs @react-navigation/bottom-tabs react-native-tab-view
    
    More
    https://reactnavigation.org/docs/tab-based-navigation

--- App.js
+++ App.js
@@ -1,10 +1,64 @@
 import React from 'react'
-import { View, StyleSheet } from 'react-native'
+import { View, StyleSheet, Platform } from 'react-native'
 import AddEntry from './components/AddEntry'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import reducer from './reducers'
 import History from './components/History'
+import { NavigationContainer } from '@react-navigation/native'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs'
+
+import { purple, white } from './utils/colors'
+import { FontAwesome, Ionicons } from '@expo/vector-icons'
+
+
+const Tabs =
+  Platform.OS === 'ios'
+    ? createBottomTabNavigator() //we show the tab at the bottom for android
+    : createMaterialTopTabNavigator() //we show normal tab for androdi at the top of the screen
+
+const HISTORY_TAB = 'History'
+const ADD_ENTRY_TAB = 'Add Entry'
+
+const TabNav = () => (
+  <Tabs.Navigator
+      initialRouteName={HISTORY_TAB}
+      screenOptions={({ route }) => ({
+          tabBarIcon: ({color, size}) => {
+              let icon;
+              switch (route.name) {
+                case HISTORY_TAB : 
+                  icon = ( <Ionicons name="ios-bookmarks" size={size} color={color}/> )
+                  break 
+                case ADD_ENTRY_TAB :
+                  icon = ( <FontAwesome name="plus-square" size={size} color={color}/> )
+                  break                 
+              }
+              return icon;
+          }
+      })}
+      tabBarOptions={{
+          header: null,
+          activeTintColor: Platform.OS === "ios" ? purple : white,
+          showIcon: true,
+          style: {
+              height: 80,
+              backgroundColor: Platform.OS === "ios" ? white : purple,
+              shadowColor: "rgba(0, 0, 0, 0.24)",
+              shadowOffset: {
+                  width: 0,
+                  height: 3
+              },
+              shadowRadius: 6,
+              shadowOpacity: 1
+          }
+      }}
+  >
+    <Tabs.Screen name={HISTORY_TAB} component={History}/>
+    <Tabs.Screen name={ADD_ENTRY_TAB} component={AddEntry} />
+  </Tabs.Navigator>
+)
 
 export default class App extends React.Component{
 
@@ -14,9 +68,11 @@ export default class App extends React.Component{
     return (
       <Provider store={this.store}>
         <View style={styles.container}>
-          <View style={ //this will get the statusbar out in the way for now
-            {height: 30}}/>
-          <History />
+          <NavigationContainer>
+            <View style={//give space for status bar
+              {height: 50}}/>
+            <TabNav />
+          </NavigationContainer>
         </View>
       </Provider>
     )
